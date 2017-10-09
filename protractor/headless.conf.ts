7d396330514832ba0948e3db345eeffc2e605cbb
ssuarezc <ssuarezc@psl.com.co>
2017-10-09 15:45:10 -0500

    correcciones con tslint

--- protractor/headless.conf.ts
+++ protractor/headless.conf.ts
@@ -1,19 +1,20 @@
 import { browser, Config } from 'protractor';
-import { reporter } from "./helpers/reporter";
+import { reporter } from './helpers/reporter';
 
 export const config: Config = {
-    framework: 'jasmine',
-    specs: ['../test/Google.spec.js'],
-    noGlobals: true,
-    SELENIUM_PROMISE_MANAGER: false,
-    capabilities: {
-        browserName: 'chrome',
-        chromeOptions: {
-            args: ['--headless', '--disable-gpu', '--window-size=800,600']
-        }
-    },
-    onPrepare: () => {
-        browser.ignoreSynchronization = true;
-        reporter();
+  framework: 'jasmine',
+  specs: ['../test/Google.spec.js'],
+  noGlobals: true,
+  SELENIUM_PROMISE_MANAGER: false,
+  capabilities: {
+    browserName: 'chrome',
+    chromeOptions: {
+      args: ['--headless', '--disable-gpu', '--window-size=800,600']
     }
+  },
+  onPrepare: () => {
+    browser.ignoreSynchronization = true;
+    reporter();
+  }
 }
+;
